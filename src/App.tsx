d582f1bf6a50945427a4336220f062063650388f
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-11-24 18:36:57 -0500

    Added Header component and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
-      <h2>App Component</h2>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <header className='header'>
+        <h2 className='header__title'>Game Sort</h2>
+      </header>
+    </>
+  );
+}
