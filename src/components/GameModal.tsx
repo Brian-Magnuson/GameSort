1148486e9f1d01a7fe9f9ef1c8d9064133d997a9
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-11-26 15:07:18 -0500

    Changed GameModal to accept GameInfo from GamesView

--- src/components/GameModal.tsx
+++ src/components/GameModal.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import GameInfo from '../GameInfo';
 
 interface GameModalProps {
   setGameSelected: React.Dispatch<React.SetStateAction<number>>
+  game: GameInfo
 }
 export default function GameModal(props: GameModalProps) {
 
@@ -9,10 +11,16 @@ export default function GameModal(props: GameModalProps) {
     props.setGameSelected(-1);
   }
 
+  const platforms = props.game.platforms != undefined && props.game.platforms.length != 0
+    ? props.game.platforms.reduce((prev, curr) => prev + ", " + curr)
+    : "";
+
   return (
     <>
       <div className='game-modal-backdrop'>
         <div className="game-modal">
+          <h1>{props.game.name}</h1>
+          <h4>{platforms}</h4>
           <button onClick={goUnsetGame}>Close</button>
         </div>
       </div>
--- src/components/GamesView.tsx
+++ src/components/GamesView.tsx
@@ -4,26 +4,49 @@ import GameModal from './GameModal';
 
 export default function GamesView() {
 
+  const gameData = [
+    {
+      index: 0,
+      name: "Super Awesome Game",
+      rating: 9,
+      ratingCount: 1024,
+      releaseDate: "11/26/2022",
+      genres: ["Action", "Adventure"],
+      platforms: ["PC", "XBOX"],
+      tags: ["2D", "Difficult"],
+      ageRatings: ["E", "3"],
+    },
+    {
+      index: 1,
+      name: "Super Cool Game",
+      rating: 8,
+      ratingCount: 200,
+      releaseDate: "11/26/2022",
+      genres: ["Action", "RPG"],
+      platforms: ["PC"],
+      tags: ["3D", "Open World"],
+      ageRatings: ["T", "12"],
+    }
+  ]
+
   const [gameSelected, setGameSelected] = React.useState(-1);
 
   return (
     <>
       <section className='games-view'>
-        {gameSelected != -1 && <GameModal setGameSelected={setGameSelected} />}
+        {gameSelected != -1 && <GameModal
+          setGameSelected={setGameSelected}
+          game={gameData[gameSelected]}
+        />
+        }
         <h3>Games matched:</h3>
         <section className='games-view__grid'>
           <GameBox
-            game={{
-              index: 1,
-              name: "Super Awesome Game",
-              rating: 9,
-              ratingCount: 1024,
-              releaseDate: "11/26/2022",
-              genres: ["Action", "Adventure"],
-              platforms: ["PC", "XBOX"],
-              tags: ["2D", "Difficult"],
-              ageRatings: ["E", "3"],
-            }}
+            game={gameData[0]}
+            setGameSelected={setGameSelected}
+          />
+          <GameBox
+            game={gameData[1]}
             setGameSelected={setGameSelected}
           />
         </section>
