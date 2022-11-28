41f2a7424d2194e37fb28121f273871b9d482b9d
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-11-28 14:43:07 -0500

    Added breaks to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -28,20 +28,25 @@ export default function Sidebar() {
         <div className="sidebar__filter-section">
           <h4>Release Date</h4>
           <label htmlFor="before-release-date">Before Date</label>
+          <br />
           <input type="date" name="before-release-date" />
           <br />
           <label htmlFor="after-release-date">After Date</label>
+          <br />
           <input type="date" name="after-release-date" />
         </div>
         <div className="sidebar__filter-section">
           <h4>Review Ratings</h4>
           <label htmlFor="at-least-rating">Lower Bound</label>
+          <br />
           <input type="number" name="at-least-rating" />
           <br />
           <label htmlFor="at-most-rating">Upper Bound</label>
+          <br />
           <input type="number" name="at-most-rating" />
           <br />
           <label htmlFor="review-count">Min Review Count</label>
+          <br />
           <input type="number" name="review-count" />
         </div>
         <button className="sidebar__submit-button">
