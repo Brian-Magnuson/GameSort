dc619eb94b3da270c7fabf274b1ae01d05ec3463
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-11-28 13:00:25 -0500

    Changed GameInfo interface and added type to gameData in GamesView.tsx

--- src/GameInfo.ts
+++ src/GameInfo.ts
@@ -10,5 +10,6 @@ export default interface GameInfo {
   platforms?: string[],
   tags?: string[],
   ageRatings?: string[],
-  imageUrl?: string
+  imageUrl?: string,
+  similarGames?: string[]
 }
--- src/components/GamesView.tsx
+++ src/components/GamesView.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import GameInfo from '../GameInfo';
 import GameBox from './GameBox';
 import GameModal from './GameModal';
 
 export default function GamesView() {
 
-  const gameData = [
+  const gameData: GameInfo[] = [
     {
       index: 0,
       name: "Super Awesome Game",
@@ -28,7 +29,7 @@ export default function GamesView() {
       tags: ["3D", "Open World"],
       ageRatings: ["T", "12"],
     }
-  ]
+  ];
 
   const [gameSelected, setGameSelected] = React.useState(-1);
 
