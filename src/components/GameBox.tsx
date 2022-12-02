d4271cc4f3248aea10c95bce049a21cca95900b1
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-12-02 17:13:43 -0500

    Added matchRating badge to GameBox

--- src/components/GameBox.tsx
+++ src/components/GameBox.tsx
@@ -25,6 +25,7 @@ export default function GameBox(props: GameBoxProps) {
     <>
       <div>
         <div style={styles} className='games-view__game-box' onClick={goSetGame}>
+          <div className='games-view__game-box-badge'>{props.matchedGame.matchRating.toFixed(2)}</div>
           {!game.imageUrl && <h4>{game.name}</h4>}
         </div>
         <h5>{game.name}</h5>
