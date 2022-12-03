998ed931655202a7fb9fb0100e0418cef77d2ecf
GraysonKorn <grayson.kornberg@gmail.com>
2022-12-02 20:45:52 -0500

    moved calc ratings function

--- src/components/GamesView.tsx
+++ src/components/GamesView.tsx
@@ -22,87 +22,11 @@ export default function GamesView(props: GamesViewProps) {
     if (props.findMatchRatingsToggle) {
       props.setFindMatchRatingsToggle(false)
       let newMatches: MatchResult[] = []
-      for (let i = 0; i < 128434; i++) {
-        let currGame = data[i.toString()]
-        let totalFilter = 0
-        let matchedFilter = 0
-        if ('afterReleaseDate' in props.formInput) {
-          totalFilter++
-          if (currGame['releaseDate'] != null) {
-            let d1 = new Date(props.formInput['afterReleaseDate'] as string)
-            let d2 = new Date(currGame['releaseDate'])
-            if (d2 >= d1) matchedFilter++
-          }
-        }
-        if ('beforeReleaseDate' in props.formInput) {
-          totalFilter++
-          if (currGame['releaseDate'] != null) {
-            let d1 = new Date(props.formInput['beforeReleaseDate'] as string)
-            let d2 = new Date(currGame['releaseDate'])
-            if (d2 <= d1) matchedFilter++
-          }
-        }
-        props.formInput['ageRatings'].forEach((rating) => {
-          totalFilter++
-          if (
-            currGame['ageRatings'] != null &&
-            rating == currGame['ageRatings']
-          ) {
-            matchedFilter++
-          }
-        })
-        props.formInput['genres'].forEach((genre) => {
-          totalFilter++
-          if (currGame['genres'] != null) {
-            let currGenres = eval(currGame['genres'])
-            if (currGenres.includes(genre)) {
-              matchedFilter++
-            }
-          }
-        })
-        props.formInput['platforms'].forEach((platform) => {
-          totalFilter++
-          if (currGame['platforms'] != null) {
-            let currPlatforms = eval(currGame['platforms'])
-            if (currPlatforms.includes(platform)) {
-              matchedFilter++
-            }
-          }
-        })
-        if (props.formInput['ratingCountAtLeast'] != null) {
-          totalFilter++
-          if (
-            currGame['ratingCount'] != null &&
-            currGame['ratingCount'] >= props.formInput['ratingCountAtLeast']
-          ) {
-            matchedFilter++
-          }
-        }
-        if (props.formInput['ratingsAtLeast'] != null) {
-          totalFilter++
-          if (
-            currGame['rating'] != null &&
-            currGame['rating'] >= props.formInput['ratingsAtLeast']
-          ) {
-            matchedFilter++
-          }
-        }
-        if (props.formInput['ratingsAtMost'] != null) {
-          totalFilter++
-          if (
-            currGame['rating'] != null &&
-            currGame['rating'] <= props.formInput['ratingsAtMost']
-          ) {
-            matchedFilter++
-          }
-        }
-        let currMatchRating = matchedFilter / totalFilter
-        newMatches.push({ matchRating: currMatchRating, index: i })
-      }
+      calculateMatchRatings(data, props.formInput, newMatches)
       console.log('done matching ratings')
-      const startTime = performance.now();
+      const startTime = performance.now()
       mergeSort(newMatches)
-      const endTime = performance.now();
+      const endTime = performance.now()
       setSortTime(endTime - startTime)
       setMatches(newMatches)
       console.log('done sorting')
