5d9bfb38fbcda6459354f9af9c58567c6a2dd652
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2022-12-03 15:48:48 -0500

    Added number of games displayed and show more games button

--- src/components/GamesView.tsx
+++ src/components/GamesView.tsx
@@ -18,6 +18,7 @@ export default function GamesView(props: GamesViewProps) {
   const data = React.useContext(GlobalContext)
   const [gameSelected, setGameSelected] = React.useState(-1)
   const [sortTime, setSortTime] = React.useState(0)
+  const [numOfGames, setNumOfGames] = React.useState(100);
 
   React.useEffect(() => {
     if (props.findMatchRatingsToggle) {
@@ -33,11 +34,17 @@ export default function GamesView(props: GamesViewProps) {
   }, [props.findMatchRatingsToggle])
   React.useEffect(() => {
     if (props.matches.length == 0) props.setFindMatchRatingsToggle(true)
+    setNumOfGames(100)
   }, [props.matches])
 
   const gameBoxes = props.matches.map((elem, index) => {
-    if (props.matches.length == 1 && props.matches[0]['index'] == -1) return
-    else if (index <= 100) {
+    if (props.matches.length == 1 && props.matches[0]['index'] == -1) {
+      return
+    }
+    if (props.matches.length < numOfGames) {
+      setNumOfGames(props.matches.length)
+    }
+    if (index <= Math.min(props.matches.length, numOfGames)) {
       return (
         <GameBox
           key={elem.index}
@@ -47,6 +54,10 @@ export default function GamesView(props: GamesViewProps) {
       )
     }
   })
+
+  const goShowMoreGames = () => {
+    setNumOfGames((prev) => prev + 100)
+  }
 
   return (
     <>
@@ -61,8 +72,23 @@ export default function GamesView(props: GamesViewProps) {
             }
           />
         )}
-        <h3>Sort Time: {sortTime.toFixed(1)} ms; Games matched:</h3>
+
+        {props.matches[0] && props.matches[0]['index'] != -1 &&
+          <div>
+            <h3>Showing {Math.min(numOfGames, props.matches.length)} of {props.matches.length} matched games</h3>
+            <h6>Sorted {props.matches.length} of {Object.keys(data).length} games</h6>
+            <h6>Sort time: {sortTime.toFixed(1)} ms</h6>
+          </div>
+        }
+        {props.matches.length == 0 &&
+          <h3>Showing 0 of 0 matched games</h3>
+        }
+
         <section className='games-view__grid'>{gameBoxes}</section>
+
+        {props.matches[0] && props.matches[0]['index'] != -1 && numOfGames != props.matches.length &&
+          <button onClick={goShowMoreGames}>Show more games</button>
+        }
       </section>
     </>
   )
